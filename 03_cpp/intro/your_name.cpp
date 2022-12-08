b2227aa6110387e9befaf935cd8ac54c8031f1fc
EkaterinaGaldina <galdina.ea@edu.spbstu.ru>
2022-12-08 19:40:58 +0300

    feat(03): add your_name

---
+++ 03_cpp/intro/your_name.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char name[40];
+	cout << "What is your name?" << endl;
+	cin >> name;
+	cout << "Hello, " << name << "!" << endl;
+	return 0;
+}
